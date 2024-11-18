3aab341e8a8d5da0391752c18776012693a75128
RenatiusvanSander <remad@web.de>
2024-11-18 14:08:16 +0100

    feature/PDF-A3: use of utility

--- src/test/java/edu/remad/tutoring2/services/pdf/PDFCreationBuilderTest.java
+++ src/test/java/edu/remad/tutoring2/services/pdf/PDFCreationBuilderTest.java
@@ -1,10 +1,14 @@
 package edu.remad.tutoring2.services.pdf;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -19,6 +23,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import edu.remad.mustangxrechnungproducer.XRechnungXmlProducer;
+import edu.remad.mustangxrechnungproducer.utilities.XRechnungXmlProducerUtilities;
+import edu.remad.tutoring2.models.AddressEntity;
+import edu.remad.tutoring2.models.InvoiceEntity;
+import edu.remad.tutoring2.models.PriceEntity;
+import edu.remad.tutoring2.models.ServiceContractEntity;
+import edu.remad.tutoring2.models.UserEntity;
+import edu.remad.tutoring2.models.ZipCodeEntity;
 import edu.remad.tutoring2.services.pdf.constants.ContentLayoutDataConstants;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,6 +48,47 @@ public class PDFCreationBuilderTest {
 		PDFCreationBuilder builder = new PDFCreationBuilder();
 		builder.contentLayoutData(List.of(contentLayoutData));
 
+		byte[] actualPdf = builder.buildAsByteArray();
+		
+		assertNotNull(actualPdf);
+	}
+	
+	@Test
+	public void createPDF3AWithXRechnungTest() throws IOException {
+		PDFCreationBuilder builder = new PDFCreationBuilder();
+		builder.contentLayoutData(List.of(contentLayoutData));
+		
+		UserEntity mockedUser = mock(UserEntity.class);
+		AddressEntity mockedAddress = mock(AddressEntity.class);
+		List<AddressEntity> addresses = List.of(mockedAddress);
+		ZipCodeEntity mockedZipCode = mock(ZipCodeEntity.class);
+		ServiceContractEntity mockedServiceContract = mock(ServiceContractEntity.class);
+		PriceEntity mockedPrice = mock(PriceEntity.class);
+		LocalDateTime invoiceCreationDate = LocalDateTime.of(2024, 3, 14, 10, 0);
+InvoiceEntity invoiceMock = mock(InvoiceEntity.class);
+
+		when(invoiceMock.getInvoiceUser()).thenReturn(mockedUser);
+		when(mockedUser.getAddresses()).thenReturn(addresses);
+		when(mockedAddress.getAddressZipCode()).thenReturn(mockedZipCode);
+		when(mockedUser.getFirstName()).thenReturn("John");
+		when(mockedUser.getLastName()).thenReturn("Doe");
+		when(mockedAddress.getAddressStreet()).thenReturn("ExampleStreet");
+		when(mockedAddress.getAddressHouseNo()).thenReturn("12");
+		when(mockedZipCode.getZipCode()).thenReturn("22359");
+		when(mockedZipCode.getZipCodeLocation()).thenReturn("Hamburg");
+		when(mockedUser.getEmail()).thenReturn("example@openweb.info");
+		when(invoiceMock.getInvoiceServiceContract()).thenReturn(mockedServiceContract);
+		when(mockedServiceContract.getServiceContractName()).thenReturn("Elektrotechnik");
+		when(mockedServiceContract.getServiceContractDescription()).thenReturn("Explanation ipsum");
+		when(invoiceMock.getPrice()).thenReturn(mockedPrice);
+		when(mockedPrice.getPrice()).thenReturn(new BigDecimal(13));
+		when(invoiceMock.getInvoiceCreationDate()).thenReturn(invoiceCreationDate);
+		when(invoiceMock.getInvoiceNo()).thenReturn(156L);
+		
+		
+		builder.XRechnung(true, XRechnungXmlProducerUtilities.ceateJUnitTestProperties(), invoiceMock);
+		
+		
 		byte[] actualPdf = builder.buildAsByteArray();
 		
 		assertNotNull(actualPdf);
